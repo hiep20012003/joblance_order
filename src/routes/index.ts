52c6d2fa90b7ec976099a3c6236e389c8eaa37a3
Hiep Nguyen <nguyendunghiep20012003@gmail.com>
2025-11-26 00:21:58 +0700

    add health route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,21 @@
-import {Application} from 'express';
-import {createVerifyGatewayRequest} from '@hiep20012003/joblance-shared';
-import {config} from '@orders/config';
-import {paymentsRoutes} from '@orders/routes/payments.route';
-import {healthRoutes} from '@orders/routes/health.route';
-import {ordersRoutes} from '@orders/routes/orders.route';
+import { Application } from 'express';
+import { createVerifyGatewayRequest } from '@hiep20012003/joblance-shared';
+import { config } from '@orders/config';
+import { paymentsRoutes } from '@orders/routes/payments.route';
+import { healthRoutes } from '@orders/routes/health.route';
+import { ordersRoutes } from '@orders/routes/orders.route';
 import seedRoutes from '@orders/routes/seed.route';
-import {initialContextMiddleware} from '@orders/middlewares/context.middleware';
-import {negotiationRoutes} from '@orders/routes/negotiation.route';
+import { initialContextMiddleware } from '@orders/middlewares/context.middleware';
+import { negotiationRoutes } from '@orders/routes/negotiation.route';
 
 const BASE_PATH = '/api/v1';
 
 export const appRoutes = (app: Application) => {
-    app.use(initialContextMiddleware);
-    app.use('', healthRoutes.routes());
-    app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), ordersRoutes.routes());
-    app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), paymentsRoutes.routes());
-    app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), negotiationRoutes.routes());
+  app.use('', healthRoutes.routes());
+  app.use(initialContextMiddleware);
+  app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), ordersRoutes.routes());
+  app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), paymentsRoutes.routes());
+  app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), negotiationRoutes.routes());
 
-    app.use('/seed', seedRoutes);
+  app.use('/seed', seedRoutes);
 };
